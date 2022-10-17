9a8a570c13cd21d1e7681257338033d8f88dbad1
tom-newbold <thomas_newbold@outlook.com>
2022-10-17 16:32:25 +0100

    improved cards data structure to accomodate more projects

--- cards.js
+++ cards.js
@@ -19,27 +19,31 @@ var cards = [
         "description": "Python implementation of both single and multi-player Othello. Tkinter interface.",
         "thumbnail": "thumbnails/Othello_Thumbnail.png",
         "subpage":"subpages/python_othello.html"
-    },
+    },{},{},{},{},
     {
-        "id": 3,
+        "id": 7,
         "heading": "Pendulums",
         "description": "Various implementations of stacked pendulums.",
         "thumbnail": "thumbnails/SpringPendulum_Thumbnail.png",
         "subpage":"subpages/javascript_pendulums.html"
     },
     {
-        "id": 4,
+        "id": 8,
         "heading": "Fractal Clock",
         "description": "A time-dependant recursive line fractal.",
         "thumbnail": "thumbnails/Fractal_Thumbnail.png",
         "subpage":"subpages/javascript_fractal.html"
     },
     {
-        "id": 5,
+        "id": 9,
         "heading": "Generative Art",
         "description": "A collection of creative coding exercises.",
         "thumbnail": "thumbnails/Squares_Thumbnail.png",
         "subpage":"subpages/javascript_generativeArt.html"
     },
-    [0,1,2,3,4,5]
+    {
+        "main": [0,1,2,7,8,9],
+        "python": [0,1,2],
+        "javascript": [7,8,9]
+    }
 ]
