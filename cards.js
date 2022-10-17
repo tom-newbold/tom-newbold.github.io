ccf3999e4c72ff068bf863317663551ab133f3ef
tom-newbold <thomas_newbold@outlook.com>
2022-10-17 23:07:42 +0100

    added mandelbrot set subpage

--- cards.js
+++ cards.js
@@ -22,42 +22,63 @@ var cards = [
     },
     {
         "id": 3,
-        "heading": "Fractals",
+        "heading": "Mandelbrot Set",
+        "description": "Python rendering of the Mandelbrot set in Tkinter.",
+        "thumbnail":"thumbnails/MandelbrotSet_Thumbnail.png",
+        "subpage":"subpages/python_mandelbrot.html"
+    },
+    {
+        "id": 4,
+        "heading": "Other Fractals",
         "description": "Python implementations of fractals, using Turtle.",
         "thumbnail":"thumbnails/PythonFractal_Thumbnail.jpg",
         "subpage":"subpages/python_fractal.html"
     },
     {
-        "id": 4,
+        "id": 5,
         "heading": "Double Pendulum",
         "description": "Python implementations of double pendulum systems. Tkinter.",
         "thumbnail":"thumbnails/DoublePendulum_Thumbnail.png",
         "subpage":"subpages/python_pendulums.html"
-    },{},{},
+    },
+    {
+        "id": 6,
+        "heading": "Text Adventures",
+        "description": "A couple of short text adventures, written in Python.",
+        "thumbnail":"",
+        "subpage":"subpages/python_textAdventures.html"
+    },
     {
         "id": 7,
+        "heading": "Game of Life",
+        "description": "Basic implementation of Conway's game of life.",
+        "thumbnail": "",
+        "subpage": "subpage/python_gameOfLife.html"
+    },
+    {
+        "id": 8,
         "heading": "Pendulums",
         "description": "Various implementations of stacked pendulums.",
         "thumbnail": "thumbnails/SpringPendulum_Thumbnail.png",
         "subpage":"subpages/javascript_pendulums.html"
     },
     {
-        "id": 8,
+        "id": 9,
         "heading": "Fractal Clock",
         "description": "A time-dependant recursive line fractal.",
         "thumbnail": "thumbnails/Fractal_Thumbnail.png",
         "subpage":"subpages/javascript_fractal.html"
     },
     {
-        "id": 9,
+        "id": 10,
         "heading": "Generative Art",
         "description": "A collection of creative coding exercises.",
         "thumbnail": "thumbnails/Squares_Thumbnail.png",
         "subpage":"subpages/javascript_generativeArt.html"
     },
     {
-        "main": [0,1,2,7,8,9],
-        "python": [0,1,2,3,4],
-        "javascript": [7,8,9]
+        "main": [0,1,2,8,9,10],
+        "python": [0,1,2,3,4,5],
+        "javascript": [8,9,10]
     }
 ]
