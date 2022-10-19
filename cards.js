f7019396328f3a9bcd1e374ffc3e5a7642776517
tom-newbold <thomas_newbold@outlook.com>
2022-10-19 21:43:55 +0100

    refactored files

--- cards.js
+++ cards.js
@@ -4,80 +4,80 @@ var cards = [
         "heading": "Maze Generator",
         "description": "Maze generation application written in Python. GCSE NEA project.",
         "thumbnail": "thumbnails/MazeGenerator_Thumbnail.png",
-        "subpage":"subpages/python_mazeGenerator.html"
+        "subpage": "subpages/python_mazeGenerator.html"
     },
     {
         "id": 1,
         "heading": "Covid Dashboard",
         "description": "Flask-powered web application displaying COVID-19 data. ECM1400 coursework project.",
         "thumbnail": "thumbnails/nhs_logo.png",
-        "subpage":"subpages/python_covidDashboard.html"
+        "subpage": "subpages/python_covidDashboard.html"
     },
     {
         "id": 2,
         "heading": "Othelo",
         "description": "Python implementation of both single and multi-player Othello. Tkinter interface.",
         "thumbnail": "thumbnails/Othello_Thumbnail.png",
-        "subpage":"subpages/python_othello.html"
+        "subpage": "subpages/python_othello.html"
     },
     {
         "id": 3,
         "heading": "Mandelbrot Set",
         "description": "Python rendering of the Mandelbrot set in Tkinter.",
-        "thumbnail":"thumbnails/MandelbrotSet_Thumbnail.png",
-        "subpage":"subpages/python_mandelbrot.html"
+        "thumbnail": "thumbnails/MandelbrotSet_Thumbnail.png",
+        "subpage": "subpages/python_mandelbrot.html"
     },
     {
         "id": 4,
         "heading": "Other Fractals",
         "description": "Python implementations of fractals, using Turtle.",
-        "thumbnail":"thumbnails/PythonFractal_Thumbnail.jpg",
-        "subpage":"subpages/python_fractal.html"
+        "thumbnail": "thumbnails/PythonFractal_Thumbnail.jpg",
+        "subpage": "subpages/python_fractal.html"
     },
     {
         "id": 5,
         "heading": "Double Pendulum",
         "description": "Python implementations of double pendulum systems. Tkinter.",
-        "thumbnail":"thumbnails/DoublePendulum_Thumbnail.png",
-        "subpage":"subpages/python_pendulums.html"
+        "thumbnail": "thumbnails/DoublePendulum_Thumbnail.png",
+        "subpage": "subpages/python_pendulums.html"
     },
     {
         "id": 6,
         "heading": "Text Adventures",
         "description": "A few of short text adventures, written in Python.",
-        "thumbnail":"thumbnails/TextAdventure_Thumbnail.ico",
-        "subpage":"subpages/python_textAdventures.html"
+        "thumbnail": "thumbnails/TextAdventure_Thumbnail.ico",
+        "subpage": "subpages/python_textAdventures.html"
     },
     {
         "id": 7,
         "heading": "Game of Life",
         "description": "Basic implementation of Conway's game of life.",
-        "thumbnail": "thumbnail/GameOfLife_Thumbnail",
-        "subpage": "subpage/python_gameOfLife.html"
+        "thumbnail": "thumbnails/GameOfLife_Thumbnail.png",
+        "subpage": "subpages/python_gameOfLife.html"
     },
     {
         "id": 8,
         "heading": "Pendulums",
         "description": "Various implementations of stacked pendulums.",
         "thumbnail": "thumbnails/SpringPendulum_Thumbnail.png",
-        "subpage":"subpages/javascript_pendulums.html"
+        "subpage": "subpages/javascript_pendulums.html"
     },
     {
         "id": 9,
         "heading": "Fractal Clock",
         "description": "A time-dependant recursive line fractal.",
         "thumbnail": "thumbnails/Fractal_Thumbnail.png",
-        "subpage":"subpages/javascript_fractal.html"
+        "subpage": "subpages/javascript_fractal.html"
     },
     {
         "id": 10,
         "heading": "Generative Art",
         "description": "A collection of creative coding exercises.",
         "thumbnail": "thumbnails/Squares_Thumbnail.png",
-        "subpage":"subpages/javascript_generativeArt.html"
+        "subpage": "subpages/javascript_generativeArt.html"
     },
     {
-        "python": [0,1,2,3,4,5,6,7],
-        "javascript": [8,9,10]
+        "python": [0, 1, 2, 3, 4, 5, 6, 7],
+        "javascript": [8, 9, 10]
     }
 ]
